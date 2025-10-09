05e128d6644bb53dd7636d4c8a068b1441b49f2e
MSenden <mario.senden@google,ail.com>
2025-10-09 10:41:16 +0200

    removed unnecessary imports

--- scripts/simulation/first_session_simulations.py
+++ scripts/simulation/first_session_simulations.py
@@ -7,7 +7,7 @@ import tomllib
 import numpy as np
 
 from src.sim_utils import initialize_simulation_classes, setup_parallel_processing, generate_stimulus_conditions, generate_time_index
-from src.anl_utils import order_parameter, compute_weighted_locking, expand_matrix, compute_firing_rate, convert_to_frequency
+from src.anl_utils import order_parameter, compute_firing_rate, convert_to_frequency
 
 from multiprocessing import Pool, Array
 
